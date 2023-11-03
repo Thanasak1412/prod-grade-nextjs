fab531e4a4938667fab28c567ee257aa699627cd
Thanasak <thanasak@soilfish.co>
2023-11-04 04:57:51 +0700

    chore: update node and config cors

--- middleware/all.ts
+++ middleware/all.ts
@@ -6,11 +6,6 @@ import auth from './auth';
 
 const middleware = nc();
 
-const corsOptions = {
-  origin: process.env.NEXT_PUBLIC_API_HOST,
-  method: 'PUT, POST, OPTIONS',
-};
-
-middleware.use(cors(corsOptions)).use(db).use(auth);
+middleware.use(cors()).use(db).use(auth);
 
 export default middleware;
